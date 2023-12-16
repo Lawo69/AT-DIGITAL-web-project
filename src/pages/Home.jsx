0c54411aafc425b15f5ed369fc9ea406eb9fc132
Supun Gunawardana <supungunawardana.me@gmail.com>
2023-12-16 20:20:03 +0530

    Hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import "./Home.css";
+import { CustomButton } from "../components/button/Button";
+
+const clickLogic = () => {
+  
+};
 
 export const Home = () => {
   return (
-    <section className='hero-section'>
-      <div className=''></div>
-
+    // Hero Section Start
+    <section className='container'>
+      <div className="hero-section">
+        <div class="hero-content">
+          <h1 class="header-title">
+            We crush your competitors, goals, and sales records - without the B.S.
+          </h1>
+          <div className="button">
+            <CustomButton text="Get free consultation" onClick={clickLogic} />
+          </div>
+        </div>
+      </div>
     </section>
-  )
+    // Hero Section End
+  );
 }
