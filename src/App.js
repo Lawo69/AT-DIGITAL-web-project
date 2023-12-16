3234b72d33d57edaf0a7656d0a7daac2c1b23607
Supun Gunawardana <supungunawardana.me@gmail.com>
2023-12-16 17:54:45 +0530

    Footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import './App.css';
+import { Footer } from "./components/footer/Footer";
 import { Navbar } from './components/navbar/Navbar';
 import { Services, Aboutus, Contactus, Careers, Home } from './pages';
 
@@ -8,12 +9,13 @@ function App() {
     <div className="App">
       <Navbar/>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
         <Route path="/aboutus" element={<Aboutus />} />
         <Route path="/contact" element={<Contactus />} />
         <Route path="/careers" element={<Careers />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Footer.css';
+
+export const Footer = () => {
+
+    return (
+        <footer class="footer">
+            <div class="row">
+                <div class="col-1">
+                <div class="logo">
+                    <img src="/img/logo.png" alt="Logo"/>
+                </div>
+                <div class="description">
+                    <p>Your goal is our target. Not anything in between. We use online marketing platforms and tools to achieve single objective - your business results.</p>
+                </div>
+                </div>
+
+                <div class="col-2">
+                    <div class="col-3">
+                    <h3 className='footer-heading'>Our Technologies</h3>
+                    <ul>
+                        <li>ReactJS</li>
+                        <li>Gatsby</li>
+                        <li>NextJS</li>
+                        <li>NodeJS</li>
+                        <li>GraphQL</li>
+                        <li>Laravel</li>
+                    </ul>
+                    </div>
+
+                    <div class="col-3">
+                    <h3 className='footer-heading'>Our Services</h3>
+                    <ul>
+                        <li><a href="">Social media Marketing</a></li>
+                        <li><a href="">Web & Mobile App Development</a></li>
+                        <li><a href="">Data & Analytics</a></li>
+                        <li><a href="">Google Marketing solutions</a></li>
+                    </ul>
+                    </div>
+                </div>
+            </div>
+            <div className='footer-bottom'>
+                <hr />
+                <div className="row-bottom">
+                    <p><a href="">Privacy Policy</a></p><p>|</p><p><a href="">Terms & Conditions</a></p>
+                </div>
+            </div>
+            
+        </footer>
+    );
+}
